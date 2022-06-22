5f76d37ca82326b291bbd0c99cf9a1dc7a2ea47e
route-2 <ruthurao@gmail.com>
2022-06-22 14:31:58 +0530

    css

--- src/common/components/Carousal.tsx
+++ src/common/components/Carousal.tsx
@@ -8,17 +8,17 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 import { Carousel } from 'react-responsive-carousel';
 
 const Carousal = () => {
-    const items = [
-        {id: 1, title: 'item #1',image:'https://i.ibb.co/q5pbzSB/1.png'},
-        {id: 2, title: 'item #2',image:'https://i.ibb.co/GJX0xr6/2.png'},
-        {id: 3, title: 'item #3',image:'https://i.ibb.co/FJS77s1/3.png'},
-        {id: 4, title: 'item #4',image:'https://i.ibb.co/4Pth1qh/4.png'},
-        {id: 5, title: 'item #5',image:'https://i.ibb.co/sJ3H2cK/5.png'}
+    const item = [
+        {id: 1, title: 'item #1',image:"https://i.ibb.co/q5pbzSB/1.png"},
+        {id: 2, title: 'item #2',image:"https://i.ibb.co/GJX0xr6/2.png"},
+        {id: 3, title: 'item #3',image:"https://i.ibb.co/FJS77s1/3.png"},
+        {id: 4, title: 'item #4',image:"https://i.ibb.co/4Pth1qh/4.png"},
+        {id: 5, title: 'item #5',image:"https://i.ibb.co/sJ3H2cK/5.png"}
       ]
    {
         return (
             <Carousel>
-                {items.map(item => <div key={item.id} > <img src={items.image}  /> </div>)}
+                {item.map(item => <div key={item.id} > <img src={item.image}  /> </div>)}
             </Carousel>
         );
     }
